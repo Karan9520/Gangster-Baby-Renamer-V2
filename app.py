9ecb0558cbeff8a027133f81a6ad437d59cd421b
Karan singh <68398500+Karan9520@users.noreply.github.com>
2024-07-08 21:11:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-#Thank you LazyDeveloper for helping me in this journey !
-#Must Subscribe On YouTube @LazyDeveloperr 
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '@LazyDeveloper'
+def hello_wprld():
+    return 'GreyMatters'
 
 
 if __name__ == "__main__":
